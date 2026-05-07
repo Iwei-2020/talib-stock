859d36195163433f94a47fa01b6f22a00d375f12
jiawei <939706822@qq.com>
2026-05-07 15:02:00 +0800

    feat(momentum): add macd family indicators

---
+++ pandas_ta_classic/momentum/mom.py
@@ -0,0 +1,75 @@
+# Momentum (MOM)
+from typing import Any
+
+from pandas import Series
+
+from pandas_ta_classic import Imports
+from pandas_ta_classic.utils import apply_fill, apply_offset, get_offset, verify_series
+from pandas_ta_classic.utils._core import _bool_param, _pos_int, nan_on_short_input
+
+
+@nan_on_short_input
+def mom(
+    close: Series,
+    length: int | None = None,
+    talib: bool | None = None,
+    offset: int | None = None,
+    **kwargs: Any,
+) -> Series | None:
+    """Indicator: Momentum (MOM)"""
+    # Validate Arguments
+    length = _pos_int(length, 10, "length")
+    close = verify_series(close, length)
+    offset = get_offset(offset)
+    mode_talib = _bool_param(talib, False, "talib")
+
+    if close is None:
+        return None
+
+    # Calculate Result
+    if Imports["talib"] and mode_talib:
+        from talib import MOM
+
+        mom = MOM(close, length)
+    else:
+        mom = close.diff(length)
+
+    # Offset
+    mom = apply_offset(mom, offset)
+
+    mom = apply_fill(mom, **kwargs)
+
+    # Name and Categorize it
+    mom.name = f"MOM_{length}"
+    mom.category = "momentum"
+
+    return mom
+
+
+mom.__doc__ = """Momentum (MOM)
+
+Momentum is an indicator used to measure a security's speed (or strength) of
+movement.  Or simply the change in price.
+
+Sources:
+    http://www.onlinetradingconcepts.com/TechnicalAnalysis/Momentum.html
+
+Calculation:
+    Default Inputs:
+        length=1
+    MOM = close.diff(length)
+
+Args:
+    close (pd.Series): Series of 'close's
+    length (int): It's period. Default: 10
+    talib (bool): If TA Lib is installed and talib is True, Returns the TA Lib
+        version. Default: False
+    offset (int): How many periods to offset the result. Default: 0
+
+Kwargs:
+    fillna (value, optional): pd.DataFrame.fillna(value)
+    fill_method (value, optional): Type of fill method
+
+Returns:
+    pd.Series: New feature generated.
+"""
